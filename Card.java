5bd2269036e04203808f8ae7de05009efc65d4da
albarho <98121172+albarho@users.noreply.github.com>
2022-01-29 00:04:31 -0500

    Add files via upload

---
+++ Card.java
@@ -0,0 +1,49 @@
+package card;
+
+public class Card {
+    public enum Suit {
+        HEARTS, CLUBS, SPADES, DIAMONDS, STAR
+      }
+    
+    public enum Value {
+            ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN,
+            EIGHT, NINE, TEN, JACK, QUEEN, KING, EMPEROR
+       }
+    
+    private Value value;
+    private Suit suit;
+    
+
+    public Card(Value value, Suit suit) {
+        this.value = value;
+        this.suit = suit;
+    }
+
+    
+    
+    public Value getValue() {
+        return this.value;
+    }
+
+    /**
+     * 
+     * @param value
+     */
+    public void setValue(Value value) {
+        this.value = value;
+    }
+
+    public Suit getSuit() {
+        return this.suit;
+    }
+
+    /**
+     * 
+     * @param suit
+     */
+    public void setSuit(Suit suit) {
+        this.suit = suit;
+    }
+
+}
+
---
+++ CardHandGenerator.java
@@ -0,0 +1,27 @@
+package card;
+
+import card.Card.Suit;
+import card.Card.Value;
+import java.util.Random;
+
+public class CardHandGenerator {
+
+
+    public static Card[] generateHand(int numCards,
+            Value[] cardValues, Suit[] cardSuits) {
+        // We'll use Random to generate random numbers
+        Random random = new Random();
+        
+        Card[] hand = new Card[numCards];
+
+        for (int i = 0; i < hand.length; i++) {
+            Card.Value value = cardValues[random.nextInt(cardValues.length)];
+            Card.Suit suit = cardSuits[random.nextInt(cardSuits.length)];
+
+            Card card = new Card(value, suit);
+            hand[i] = card;
+        }
+        return hand;
+    }
+
+}
